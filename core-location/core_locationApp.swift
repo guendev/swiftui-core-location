a7db35724eef1124f616b258fa48008418ba4f7c
Guen <heloo@guen.dev>
2023-08-29 13:08:52 +0700

    Initial Commit

---
+++ core-location/core_locationApp.swift
@@ -0,0 +1,17 @@
+//
+//  core_locationApp.swift
+//  core-location
+//
+//  Created by Guen on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct core_locationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
